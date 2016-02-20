6c01ab1cd4f0a5e4dad82929eb04d79f2d603563
--replace-all <diaconu.codrin@gmail.com>
2016-02-20 15:19:14 -0500

    Added custom message event

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,12 @@
+//Our front end JavaScript
+
 var socket = io();
 
 //This will log on the browser console
 socket.on("connect", ()=>{
     console.log("Connected to socket.io server");
+});
+//Listen to the emit. First argument is the emit name, second argument is the callback with our data.
+socket.on("message", (data)=>{
+    console.log(data);
 });
--- server.js
+++ server.js
@@ -10,8 +10,24 @@ var io = require("socket.io")(http);
 app.use(express.static(__dirname + "/public"));
 
 //Tell the server to wait for a connection and when a connection is detected print to the console.
-io.on("connection", ()=>{
+io.on("connection", (socket)=>{
+//The socket in the callback is an individual connection.
     console.log("User connected via socket.io");
+
+    //Listen on the message event
+    socket.on("message", (data)=>{
+        console.log(data);
+        //socket.broadcast will emit to everyone but the user the sent the message.
+        //if we wanted to emit to everyone, including the user that sent the message we would use io.broadcast
+        //Again we pass the emit method two argument s the first is the type and the second is the data that we received.
+        socket.broadcast.emit("message", data);
+    });
+
+    //.emit takes two arguments. The first is the name which can be anything we want.
+    //The second argument is data to send. It is recommended to use an object.
+    socket.emit("message", {
+       text: "Welcome to this most awesome chat application"
+    });
 });
 
 http.listen(PORT, ()=>{
