cf19eb21c26fe61117d2f4a69f93609d76a9f3f6
--replace-all <diaconu.codrin@gmail.com>
2016-02-20 14:36:50 -0500

    Created basic connection

--- server.js
+++ server.js
@@ -4,8 +4,15 @@ var app = express();
 //This tells Node to start a new server and to use the express app as a boilerplate.
 var http = require("http").Server(app);
 
+var io = require("socket.io")(http);
+
 //We are exposing a folder
 app.use(express.static(__dirname + "/public"));
+
+//Tell the server to wait for a connection and when a connection is detected print to the console.
+io.on("connection", function(){
+    console.log("User connected via socket.io");
+});
 
 http.listen(PORT, ()=>{
     console.log(`Server started on port ${PORT}`)
